a831d29f8668f86ad4c72da0d3d4da83e59b5e7a
Rémi DUJARDIN <remi1.dujardin@univ-lille1.fr>
2017-06-06 15:01:13 +0200

    Modification de Bridge

--- Conception/src/business/Bridge.java
+++ Conception/src/business/Bridge.java
@@ -1,61 +1,65 @@
+package business;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-import business.Admin;
-import business.Article;
-import business.Client;
-
 public class Bridge {
 
-	private Connection con;
-	
+	private static Connection con;
+
 	/**
 	 * Creer la connection avec la bdd
-	 * @throws ClassNotFoundException 
+	 * 
+	 * @throws ClassNotFoundException
 	 */
-	Bridge() throws Exception{
+	public Bridge() throws Exception {
 		Class.forName("org.postgresql.Driver");
-		con = DriverManager.getConnection("jdbc:postgresql://psqlserv/n2p1?allowMultiQueries=true","barbetf","moi");
+		con = DriverManager.getConnection("jdbc:postgresql://psqlserv/n2p1?allowMultiQueries=true", "barbetf", "moi");
 	}
-	
+
 	/**
 	 * Transforme les articles de la bdd en objet Article
-	 * @param ref les references vers les articles
+	 * 
+	 * @param ref
+	 *            les references vers les articles
 	 * @return un tableau d'article
 	 */
-	Article[] getArticles(int ... ref){
+	public static Article[] getArticles(int... ref) {
 		return null;
 	}
-	
-	Article[] getAllArticles(){
+
+	public static Article[] getAllArticles() {
 		return null;
 	}
-	
+
 	/**
 	 * Transforme un client de la bdd en objet Client
+	 * 
 	 * @param ID
 	 * @return un objet Client
 	 */
-	Client getClient(int ID){
+	public static Client getClient(int ID) {
 		return null;
 	}
-	
+
 	/**
 	 * Transforme un client de la bdd en objet Admin
+	 * 
 	 * @param ID
 	 * @return un objet Admin
 	 */
-	Admin getAdmin(int ID){
+	public static Admin getAdmin(int ID) {
 		return null;
 	}
-	
+
 	/**
 	 * Transforme une commande de la bdd en objet Order
+	 * 
 	 * @param ID
 	 * @return un objet Order
 	 */
-	Order getOrder(int ID){
+	public static Order getOrder(int ID) {
 		return null;
 	}
-	
+
 }

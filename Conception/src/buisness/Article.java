98625fcc44ae08e5ff664bbac5aaf061a59c1518
Rémi DUJARDIN <remi1.dujardin@etudiant.univ-lille1.fr>
2017-06-06 13:01:45 +0200

    Creation de Article

---
+++ Conception/src/buisness/Article.java
@@ -0,0 +1,16 @@
+package buisness;
+
+public class Article {
+	int reference;
+	String name;
+	String description;
+	String image_url;
+	String brand;
+	String category;
+	double price;
+	double discount;
+
+	public Article() {
+
+	}
+}

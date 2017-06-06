777b8544fc615b6a9f8e922642d9f9dead75c330
Nicolas SURMONT <nicolas.surmont@etudiant.univ-lille1.fr>
2017-06-06 12:58:51 +0200

    Article

--- Conception/src/metier/Article.java
+++ Conception/src/metier/Article.java
@@ -2,4 +2,14 @@ package metier;
 
 public class Article {
 
+	public int getPrice() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getRef() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
--- Conception/src/metier/Cart.java
+++ Conception/src/metier/Cart.java
@@ -1,9 +1,31 @@
 package metier;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Cart {
 
-	Map<Integer, Integer> cartMap;
+	Map<Integer, Article> cartMap;
 			
+	public Cart(){
+		cartMap = new HashMap<Integer, Article>();
+	}
+	
+	public double getTotalPrice(){
+		double total = 0.0;
+		for(Article a : cartMap.values()){
+			total =+ a.getPrice();
+		}
+		return total;
+	}
+	
+	public boolean Remove(int ref){
+		for(Article a : cartMap.values()){
+			if(a.getRef()==ref){
+				cartMap.remove(ref);
+				return true;
+			}
+		}
+		return false;
+	}
 }

9478d1d13c0a7c3ff79a159c41875eda09b56e31
Rémi DUJARDIN <remi1.dujardin@univ-lille1.fr>
2017-06-06 14:57:29 +0200

    Ajout de Order et Main

--- Conception/src/main/Main.java
+++ Conception/src/main/Main.java
@@ -1,9 +1,11 @@
 package main;
 
+import business.Client;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Client marilyn_manson = new Client("Marilyn", "Manson", "marilyn.manson@gmail.com", "0666666666", "abcd1234");
 
 	}
-
 }

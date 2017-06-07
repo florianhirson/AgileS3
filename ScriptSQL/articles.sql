a0423b0e5dc53bbe98df6950936fa8a20036e0a8
Thomas MASTALERZ <thomas.mastalerz@etudiant.univ-lille1.fr>
2017-06-07 14:22:18 +0200

    Upload new file

---
+++ ScriptSQL/articles.sql
@@ -0,0 +1,17 @@
+INSERT INTO article(libelle,description,category,stock,prix,image) VALUES('AK 47','Puissant et fiable, l AK-47 est l un des fusils d assaut les plus populaires au monde. Il est tres mortel dans des rafales controlees.','Autodefense',1400,2700,'https://vignette4.wikia.nocookie.net/cswikia/images/6/66/Ak47go.png');
+
+INSERT INTO article(libelle,description,category,stock,prix,image) VALUES('Desert Eagle','Aussi puissant que couteux, le Desert Eagle est un pistolet emblematique difficile a maitriser, mais etonnamment precis a longue distance.','Autodefense',16400,700,'http://www.imfdb.org/images/thumb/e/ea/DesertEagle50AE.jpg');
+
+INSERT INTO article(libelle,description,category,stock,prix,image) VALUES('Super Rateau','Un super rateau pour super rateler votre super jardin de ouf guedin','Jardinage',5400,19.99,'https://www.leroymerlin.fr/multimedia/d21400296059/produits/rateau-a-gazon-leborgne-l-150-cm.jpg');
+
+INSERT INTO article(libelle,description,category,stock,prix,image) VALUES('T34 Russe','Cyka blyat','Transport',400,4.99,'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Char_T-34.jpg/280px-Char_T-34.jpg');
+
+INSERT INTO article(libelle,description,category,stock,prix,image) VALUES('Bouteille vide','Bouteille','divers',80000,50.09,'http://www.logementtunisie.com/images/upload/inconnu.jpg');
+
+INSERT INTO article(libelle,description,category,stock,prix,image) VALUES('Biscuit','Un biscuit','Alimentaire',70000,7.00,'http://www.logementtunisie.com/images/upload/inconnu.jpg');
+
+INSERT INTO article(libelle,description,category,stock,prix,image) VALUES('Livre pendu','You died','Culture',50,40.00,'http://www.logementtunisie.com/images/upload/inconnu.jpg');
+
+INSERT INTO article(libelle,description,category,stock,prix,image) VALUES('Manuel de survie face au poubelle','Wut ?','Culture',500,80.00,'http://www.logementtunisie.com/images/upload/inconnu.jpg');
+
+INSERT INTO article(libelle,description,category,stock,prix,image) VALUES('Belvedere Pure Vodka','Vodka','Alcool',6400,41.60,'https://statics.monoprix.fr/course/g_1774679_pure-vodka.jpg');
